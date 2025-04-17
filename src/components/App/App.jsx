b51c561113c77fe43b16cf859584ce219827b82c
ontruster74 <ontruster74@gmail.com>
2025-04-17 10:56:35 -0700

    basic router structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,14 +8,18 @@ import '../CharacterVault/CharacterVault'
 import '../NotFound/NotFound'
 
 function App() {
-  
+  // Header will always be visible, and hold a NavLink button for LandingPage and CharacterVault
+  // FormView will hold state and pass props to SheetView
+  // SheetView will probably be the only component that needs fetch calls to an API
+  // FormView or SheetView will save characters to the backend
+  // CharacterVault will make GET requests to the backend
   return (
     <main>
       <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/form" element={<FormView />} />
-        <Route path="/sheet" element={<SheetView />} />
+        <Route path="/sheet" element={<SheetView />} /> 
         <Route path="/vault" element={<CharacterVault />} />
         <Route path="*" element={<NotFound />} />
       </Routes>

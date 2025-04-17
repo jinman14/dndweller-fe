4d832d11cbed6a7b8cde3388a11352baa3613421
ontruster74 <ontruster74@gmail.com>
2025-04-17 10:44:06 -0700

    rearrange file structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import { reactRouter as Routes, Route } from 'router-react-dom'
+import './App.css'
+import '../Header/Header'
+import '../LandingPage/LandingPage'
+import '../FormView/FormView'
+import '../SheetView/SheetView'
+import '../CharacterVault/CharacterVault'
+import '../NotFound/NotFound'
+
+function App() {
+  
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/form" element={<FormView />} />
+        <Route path="/sheet" element={<SheetView />} />
+        <Route path="/vault" element={<CharacterVault />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './components/App/App.jsx'
+import { BrowserRouter } from 'router-react-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 )

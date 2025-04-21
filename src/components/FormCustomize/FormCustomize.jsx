58f20c62765e777a11fabc524514b39aef30d071
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-04-20 23:03:20 -0700

    added  Character Cantrip selection

--- src/components/FormCustomize/FormCustomize.jsx
+++ src/components/FormCustomize/FormCustomize.jsx
@@ -11,6 +11,18 @@ function FormCustomize() {
     const BASE_STAT_VALUE = 8
     const MAX_STAT_POINTS_PER_STAT = 5
     const [confirmedStats, setConfirmedStats] = useState(false)
+    const [availableSkills, setAvailableSkills] = useState([])
+    const [selectedSkills, setSelectedSkills] = useState({})
+    const [skillPointsLeft, setSkillPointsLeft] = useState(5)
+    const MAX_POINTS_PER_SKILL = 3;
+    const [confirmedSkill, setConfirmedSkill] = useState(false)
+    const [availableCantrips, setAvailableCantrips] = useState([])
+    const [availableSpells, setAvailableSpells] = useState([])
+    const [selectedCantrips, setSelectedCantrips] = useState([])
+    const [selectedSpells, setSelectedSpells] = useState([])
+    const [confirmedCantrips, setConfirmedCantrips] = useState(false);
+    const [confirmedSpells, setConfirmedSpells] =useState(false)
+
     
 
     useEffect(() => {
@@ -22,6 +34,37 @@ function FormCustomize() {
             const classStats = data
             // console.log("Stats LOADED:", classStats)
             setAvailableStats(classStats || [])
+        })
+    }, [selectedToken])
+
+    useEffect(() => {
+        if(!selectedToken) return
+
+        fetch('/skills_data.json')
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log("Skills loaded:", data);
+            // console.log("Selected token class:", selectedToken.class);
+
+            const filteredSkills = data.filter((skill) => {
+                return skill.recommendedFor.includes(selectedToken.class)
+            })
+            setAvailableSkills(filteredSkills)
+        })
+    }, [selectedToken])
+
+    useEffect(() => {
+        if(!selectedToken) return
+
+        fetch('/spells_n_cantrips_data.json')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Selected class:", selectedToken.class)
+            console.log("Available cantrips before filter:", data.cantrips)
+            const filterCantrips = data.cantrips.filter((cantrip) => {
+                return cantrip.recommendedFor.includes(selectedToken.class)
+            })
+            setAvailableCantrips(filterCantrips)
         })
     }, [selectedToken])
 
@@ -55,7 +98,9 @@ function FormCustomize() {
                             <div className="stat-card" key={stat.name}>
                                 <h4>{stat.name} - {BASE_STAT_VALUE + assignedPoints}</h4>
                                 <p>{stat.description}</p>
-                                <p><strong>Recommended for:</strong> {stat.recommendedFor.join(", ")}</p>
+                                {!confirmedStats && (
+                                  <p><strong>Recommended for:</strong> {stat.recommendedFor.join(", ")}</p>
+                                )}
                             
                                 {!confirmedStats && (
                                     <div className="stat-buttons">
@@ -102,7 +147,92 @@ function FormCustomize() {
                 </div>
             )}
 
-            <div className='form-selection'></div> {/* Skills */}
+            {confirmedStats && (
+                <div className='form-selection'>{/* Skills */}
+                    {availableSkills.map(skill => {
+                        const assignedSkillPoints = selectedSkills[skill.name] || 0
+                        return (
+                            <div key={skill.name} className="skill-card">
+                                <h4>{skill.name} - {assignedSkillPoints}</h4>
+                                <p>{skill.description}</p>
+                                <div className="skill-buttons">
+                                    {/* - Button */}
+                                    <button
+                                            onClick={() => {
+                                                if (assignedSkillPoints > 0) {
+                                                    setSelectedSkills(previousState => ({
+                                                        ...previousState,
+                                                        [skill.name]: assignedSkillPoints - 1
+                                                    }))
+                                                    setSkillPointsLeft(previousState => previousState + 1)
+                                                }
+                                            }}
+                                            disabled={assignedSkillPoints === 0}
+                                        >–</button>
+
+                                        {/* + Button */}
+                                        <button
+                                            onClick={() => {
+                                                if (skillPointsLeft > 0 && assignedSkillPoints < MAX_POINTS_PER_SKILL) {
+                                                    setSelectedSkills(previousState => ({
+                                                        ...previousState,
+                                                        [skill.name]: assignedSkillPoints + 1
+                                                    }))
+                                                    setSkillPointsLeft(previousState => previousState - 1)
+                                                }
+                                            }}
+                                            disabled={skillPointsLeft === 0 || assignedSkillPoints >= MAX_POINTS_PER_SKILL}
+                                        >+</button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <p>Points Remaining: {skillPointsLeft}</p>
+                    <button
+                        className="confirm-button"
+                        onClick={() => setConfirmedSkill(true)}
+                        disabled={skillPointsLeft > 0}>
+                            Confirm Choices
+                    </button>
+                </div>
+            )}
+            
+            {confirmedSkill && !confirmedCantrips && (
+                <div className='form-selection'> {/* Cantrips */}
+                    <h3>Select Your Cantrips, Hero</h3>
+                        {availableCantrips.map((cantrip) => {
+                            const isSelected = selectedCantrips.includes(cantrip.name)
+
+                            return (
+                                <div key={cantrip.name} className={`cantrip-card ${isSelected ? 'selected' : ''}`}>
+                                <h4>{cantrip.name}</h4>
+                                <p><strong>Range:</strong> {cantrip.range}</p>
+                                <p><strong>Casting Time:</strong> {cantrip.castingTime}</p>
+                                <p>{cantrip.description}</p>
+
+                                <button onClick={() => {
+                                    if (isSelected) {
+                                    setSelectedCantrips(previousState => previousState.filter(name => name !== cantrip.name));
+                                    } else if (selectedCantrips.length < 3) {
+                                    setSelectedCantrips(previousState => [...previousState, cantrip.name])
+                                    }
+                                }}>
+                                    {isSelected ? 'Unselect' : 'Select'}
+                                </button>
+                            </div>
+                        );
+                    })}
+                    <p>Cantrips Selected: {selectedCantrips.length} / 3</p>
+                    <button
+                        className="confirm-button"
+                        onClick={() => setConfirmedCantrips(true)}
+                        disabled={selectedCantrips.length !== 3}>
+                        
+                        Confirm Cantrips
+                    </button>
+                </div>
+            )}
+
             <div className='form-selection'></div> {/* Spells */}
             <div className='form-selection'></div> {/* Name */}
             <div className='form-selection submit-button'></div> {/* Submit */}

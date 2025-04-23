ed92aad1f38784cec5675b794619512cc70a89b3
ontruster74 <ontruster74@gmail.com>
2025-04-23 00:40:11 -0700

    add additional links

--- src/components/ResourcesPage/ResourcesPage.jsx
+++ src/components/ResourcesPage/ResourcesPage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './ResourcesPage.css';
 
 const resources = [
+  { name: 'GitHub Repository (Frontend)', url: 'https://github.com/jinman14/dndweller-fe' },
+  { name: 'GitHub Repository (Backend)', url: 'https://github.com/jinman14/dndweller-be' },
+  { name: '5e SRD API', url: 'https://www.dnd5eapi.co/' },
+  { name: '5e Character Sheet (Fillable PDF)', url: 'https://media.wizards.com/2016/dnd/downloads/5E_CharacterSheet_Fillable.pdf' },
   { name: 'D&D Beyond', url: 'https://www.dndbeyond.com/' },
   { name: 'Roll20', url: 'https://roll20.net/' },
   { name: 'Owlbear Rodeo', url: 'https://www.owlbear.rodeo/' },
@@ -10,6 +14,12 @@ const resources = [
   { name: 'Fantasy Name Generators', url: 'https://www.fantasynamegenerators/' },
   { name: 'r/DnD (Reddit)', url: 'https://www.reddit.com/r/DnD' },
   { name: 'Tabletop Audio', url: 'https://www.tabletopaudio.com/' },
+  { name: 'DriveThruRPG', url: 'https://www.drivethrurpg.com/en/' },
+  { name: 'DMs Guild', url: 'https://www.dmsguild.com/' },
+  { name: 'Fantasy Map Generator by Azgaar', url: 'https://azgaar.github.io/Fantasy-Map-Generator/' },
+  { name: 'DnD 5e Wikidot', url: 'https://dnd5e.wikidot.com/' },
+  { name: 'Hero Forge', url: 'https://www.heroforge.com/' },
+  { name: 'Token Stamp / Dice Roller', url: 'https://rolladvantage.com/tokenstamp/' }
 ]
 
 function ResourcesPage() {
@@ -20,6 +30,7 @@ function ResourcesPage() {
           {resources.map((res) => (
             <li key={res.url}>
               <a href={res.url} target="_blank" rel="noopener noreferrer">
+                {res.name}
               </a>
             </li>
           ))}

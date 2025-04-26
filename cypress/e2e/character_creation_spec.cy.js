f8b254153e613e3a302b2b5c2bb6735a989a40fb
Jacob Inman <jrinman6295@gmail.com>
2025-04-25 17:08:49 -0700

    cypress walks through to the gender form page

--- cypress/e2e/character_creation_spec.cy.js
+++ cypress/e2e/character_creation_spec.cy.js
@@ -1,5 +1,5 @@
 describe('character creation', () => {
-  it('can access the race class selection screen', () => {
+  it('can create a character', () => {
     cy.visit('http://localhost:5173/');
     cy.contains('a', 'Create Your Dungeon Dweller').click();
     cy.contains("Choose A Class:");
@@ -7,6 +7,9 @@ describe('character creation', () => {
     cy.contains('Bard').click();
     cy.contains('Tiefling').click();
     cy.contains('a', "Fill Out Your Dweller's Details").click();
-
+    cy.get('.token-grid').children().should('have.length.greaterThan', 0);
+    cy.get('.token-card').contains('Tiefling').click();
+    cy.contains('button', 'Confirm').click();
+    cy.url().should('include', '/form');
   })
 })
